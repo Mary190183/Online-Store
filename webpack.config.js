36b17a5531a83064698b96fd4c6e9c68a1a543ad
balabanova-iu <balabanova.iu@gmail.com>
2022-12-15 15:53:57 +0300

    feat: setup html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
+    index: path.resolve(__dirname, "./src/index.js"),
+    item: path.resolve(__dirname, "./src/item.js"),
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/item.html"),
+      filename: "item.html",
+      chunks: ["item"],
+    }),
+  ],
 };
