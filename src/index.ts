966c118306dbe72ab09121d66ba245ac0dc18920
Mariya Petrovskaya <pma190183@yandex.ru>
2022-12-16 19:32:37 +0300

    feat: add assets dir and arror in index.ts svg

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ import "./index.css";
 console.log("Index");
 
 
+const myGitHub = document.querySelector('.github') as HTMLImageElement;
+
+const myRsschool = document.querySelector('.rsschool') as HTMLImageElement;
+
+    myGitHub.src = "./assets/icon/github.svg";
+
+    myRsschool.src = "./assets/icon/rsschool.svg";
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,73 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-
+let mode = 'development';
+// const { HtmlSvgPlugin } = require("@toba/html-webpack-inline-svg");
 module.exports = {
-  mode: "development",
+  // mode: "development",
   devtool: "inline-source-map",
   entry: {
     index: path.resolve(__dirname, "./src/index.ts"),
     item: path.resolve(__dirname, "./src/item.ts"),
   },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: ["style-loader", "css-loader"],
+  //     },
+  //     {
+  //       test: /\.ts$/,
+  //       use: "ts-loader",
+  //       exclude: /node_modules/,
+  //     },
+  //   ],
+  // },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        { test: /\.txt$/, use: 'raw-loader' },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+               //Шрифты
+  
+    {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+
+        // изображения
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: mode === 'production' ? 'asset' : 'asset/resource'
+        },
+      //  шрифты и SVG
+          {
+              test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+              type: mode === 'production' ? 'asset' : 'asset/inline'
+          },
+          //аудио
+          {
+           
+
+            test: /\.(?:mp3|wav|ogg|mp4)$/i,
+            use: "file-loader",
+            type: mode === 'production' ? 'asset' : 'asset/resource'
+            // generator: {
+            //   filename: 'assets/sounds/[hash][ext]',
+            // },
+          },
     ],
-  },
+},
   resolve: {
     extensions: [".ts", ".js"],
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
+    assetModuleFilename: 'assets/[name][ext][query]',
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -43,9 +83,10 @@ module.exports = {
       chunks: ["item"],
     }),
     new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: path.resolve(__dirname, "./assets"), to: path.resolve(__dirname, "./dist/assets") }],
-    }),
+    // new CopyPlugin({
+    //   patterns: [{ from: path.resolve(__dirname, "./assets"), to: path.resolve(__dirname, "./dist/assets") }],
+    // }),
+    // new HtmlSvgPlugin()
   ],
   devServer: {
     static: path.resolve(__dirname, "./dist"),
