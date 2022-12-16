392e7271dd0f48dd5cd04c55aaf919a19a8ec930
Mariya Petrovskaya <pma190183@yandex.ru>
2022-12-16 19:58:05 +0300

    feat: background and en/ru

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import "./index.css";
 console.log("Index");
 
 
-const myGitHub = document.querySelector('.github') as HTMLImageElement;
+// const myGitHub = document.querySelector('.github') as HTMLImageElement;
 
-const myRsschool = document.querySelector('.rsschool') as HTMLImageElement;
+// const myRsschool = document.querySelector('.rsschool') as HTMLImageElement;
 
-    myGitHub.src = "./assets/icon/github.svg";
+//     myGitHub.src = "./assets/icon/github.svg";
 
-    myRsschool.src = "./assets/icon/rsschool.svg";
+//     myRsschool.src = "./assets/icon/rsschool.svg";
 
