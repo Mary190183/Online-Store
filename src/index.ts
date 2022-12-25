7e184eceaa9d7f5b045208ff77ee498c88b1dbc9
Mariya Petrovskaya <pma190183@yandex.ru>
2022-12-25 23:28:51 +0300

    feat: add filters on ruler

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ const main = body.appendChild(document.createElement(`main`)) as HTMLElement;
 const containerFiltres = main.appendChild(document.createElement(`div`)) as HTMLDivElement;
 containerFiltres.classList.add('container-filtres');
 
+
+const buttonsContainer = containerFiltres.appendChild(document.createElement(`div`)) as HTMLDivElement;
+const buttonResert = buttonsContainer.appendChild(document.createElement(`button`)) as HTMLButtonElement;
+buttonsContainer.classList.add('buttons-container')
+buttonResert.classList.add('button-filtres');
+buttonResert.textContent = 'Resert filtres'
+const buttonCopy= buttonsContainer.appendChild(document.createElement(`button`)) as HTMLButtonElement;
+buttonCopy.classList.add('button-filtres');
+buttonCopy.textContent = 'Copy link'
 const filterCategory = containerFiltres.appendChild(document.createElement(`div`)) as HTMLDivElement;
 filterCategory.classList.add('filter-category');
 const filterCategoryh2 = filterCategory.appendChild(document.createElement(`h2`)) as HTMLHeadingElement;
@@ -31,12 +40,15 @@ const filterSortTealabel = filterCategoryForm.appendChild(document.createElement
 filterSortTealabel.classList.add('filter-label');
 
 }
+
 const filterProvinceTea = containerFiltres.appendChild(document.createElement(`div`)) as HTMLDivElement;
 filterProvinceTea.classList.add('filter-privince-tea');
 const filterProvinceTeah2 = filterProvinceTea.appendChild(document.createElement(`h2`)) as HTMLHeadingElement;
 filterProvinceTeah2.classList.add('filter-h2');
 filterProvinceTeah2.textContent = 'Province';
-for (let i = 4; i < 8; i ++) {
+
+for (let i = 0; i < 4; i ++) {
+
 const filterProvinceTeaForm = filterProvinceTea.appendChild(document.createElement(`form`)) as HTMLFormElement;
 filterProvinceTeaForm.classList.add('filter-form');
 const filterProvinceTeaInput = filterProvinceTeaForm.appendChild(document.createElement(`input`)) as HTMLInputElement;
@@ -44,9 +56,88 @@ filterProvinceTeaInput.classList.add('filter-input');
 filterProvinceTeaInput.type = 'checkbox'
 const filterProvinceTealabel = filterProvinceTeaForm.appendChild(document.createElement(`label`)) as HTMLLabelElement;
 filterProvinceTealabel.classList.add('filter-label');
+
 }
 
+const filterPrice = containerFiltres.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPrice.classList.add('filter-price');
+const filterPriceh2 = filterPrice.appendChild(document.createElement(`h2`)) as HTMLHeadingElement;
+filterPriceh2.classList.add('filter-h2');
+filterPriceh2.textContent = 'Price';
+const filterPriceRange = filterPrice.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPriceRange.classList.add('filter-range');
 
+const filterPriceRange1 = filterPriceRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPriceRange1.classList.add('filter-range-points-1');
+filterPriceRange1.textContent = '0';
+
+const filterPriceRange2 = filterPriceRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPriceRange2.classList.add('filter-range-points');
+filterPriceRange2.textContent = '⟷';
+
+const filterPriceRange3 = filterPriceRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPriceRange3.classList.add('filter-range-points-2');
+filterPriceRange3.textContent = '7 $';
+
+const filterPriceRangeInput = filterPrice.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterPriceRangeInput.classList.add('filter-range-input');
+const filterPriceRangeInput1 = filterPriceRangeInput.appendChild(document.createElement(`input`)) as HTMLInputElement;
+filterPriceRangeInput1.classList.add('filter-input-range-1');
+
+
+
+
+filterPriceRangeInput1.type = 'range'
+filterPriceRangeInput1.min = "0";
+filterPriceRangeInput1.max = "7";
+filterPriceRangeInput1.value = '1';
+
+const filterPriceRangeInput2 = filterPriceRangeInput.appendChild(document.createElement(`input`)) as HTMLInputElement;
+filterPriceRangeInput2.classList.add('filter-input-range-2');
+filterPriceRangeInput2.type = 'range'
+filterPriceRangeInput2.min = "0";
+filterPriceRangeInput2.max = "6";
+filterPriceRangeInput2.value = '5';
+
+const filterStock = containerFiltres.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStock.classList.add('filter-stock');
+const filterStockh2 = filterStock.appendChild(document.createElement(`h2`)) as HTMLHeadingElement;
+filterStockh2.classList.add('filter-h2');
+filterStockh2.textContent = 'Stock';
+const filterStockRange = filterStock.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStockRange.classList.add('filter-range');
+
+const filterStockRange1 = filterStockRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStockRange1.classList.add('filter-range-points-1');
+filterStockRange1.textContent = '0';
+
+const filterStockRange2 = filterStockRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStockRange2.classList.add('filter-range-points');
+filterStockRange2.textContent = '⟷';
+
+const filterStockRange3 = filterStockRange.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStockRange3.classList.add('filter-range-points-2');
+filterStockRange3.textContent = '24';
+
+const filterStockRangeInput = filterStock.appendChild(document.createElement(`div`)) as HTMLDivElement;
+filterStockRangeInput.classList.add('filter-range-input');
+const filterRangeInput1 = filterStockRangeInput.appendChild(document.createElement(`input`)) as HTMLInputElement;
+filterRangeInput1.classList.add('filter-input-range-1');
+
+
+
+
+filterRangeInput1.type = 'range'
+filterRangeInput1.min = "0";
+filterRangeInput1.max = "24";
+filterRangeInput1.value = '2';
+
+const filterRangeInput2 = filterStockRangeInput.appendChild(document.createElement(`input`)) as HTMLInputElement;
+filterRangeInput2.classList.add('filter-input-range-2');
+filterRangeInput2.type = 'range'
+filterRangeInput2.min = "0";
+filterRangeInput2.max = "24";
+filterRangeInput2.value = '20';
 const filterCategoryCheckboxInput = document.querySelectorAll('.filter-input') as NodeListOf<HTMLInputElement>;
 const filterCategoryCheckboxLabel= document.querySelectorAll('.filter-label') as NodeListOf<HTMLLabelElement>;
 
@@ -85,11 +176,42 @@ filterCategoryCheckboxLabel[7].textContent = 'Hong Pao';
 
 const containerTeaCards = main.appendChild(document.createElement(`div`)) as HTMLDivElement;
 containerTeaCards.classList.add('container_tea-cards');
-// const containerTeaCards = document.querySelector('.container_tea-cards') as HTMLElement;
 const teaImage = containerTeaCards.appendChild(document.createElement(`div`)) as HTMLDivElement;
 teaImage.classList.add('tea-image');
+
+
 const teaFiltersRuler = containerTeaCards.appendChild(document.createElement(`div`)) as HTMLDivElement;
 teaFiltersRuler.classList.add('tea-filters-ruler');
+
+const teaFiltersRulerPriceHigh = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerPriceHigh.classList.add('tea-filters-ruler-item');
+teaFiltersRulerPriceHigh.textContent = 'Price ↑';
+
+const teaFiltersRulerPriceLow = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerPriceLow.classList.add('tea-filters-ruler-item');
+teaFiltersRulerPriceLow.textContent = 'Price ↓';
+
+const teaFiltersRulerRatingHigh = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerRatingHigh.classList.add('tea-filters-ruler-item');
+teaFiltersRulerRatingHigh.textContent = 'Rating ↑';
+
+const teaFiltersRulerRatingLow = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerRatingLow.classList.add('tea-filters-ruler-item');
+teaFiltersRulerRatingLow.textContent = 'Rating ↓';
+
+const teaFiltersRulerList = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerList.classList.add('tea-filters-ruler-item');
+const teaFiltersRulerListSvg = teaFiltersRulerList.appendChild(document.createElement(`div`)) as HTMLDivElement;
+teaFiltersRulerListSvg.classList.add('tea-filters-ruler-item-list');
+
+
+const teaFiltersRulerTiles = teaFiltersRuler.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
+teaFiltersRulerTiles.classList.add('tea-filters-ruler-item');
+const teaFiltersRulerTilesSvg = teaFiltersRulerTiles.appendChild(document.createElement(`div`)) as HTMLDivElement;
+teaFiltersRulerTilesSvg.classList.add('tea-filters-ruler-item-tiles');
+
+
+
 
 for (let i = 0; i < 24; i++) {
 
@@ -97,28 +219,27 @@ for (let i = 0; i < 24; i++) {
   teaCardInfo.classList.add('tea-card-info');
 
   filterCategoryCheckboxInput[0].addEventListener('click', () => {
-    
-          if (i > 5) {
-          teaCardInfo.classList.toggle('hidden')
-        }   
+    if (i > 5) {
+      teaCardInfo.classList.toggle('hidden')
+    }   
   })
   filterCategoryCheckboxInput[1].addEventListener('click', () => {
-   
-        if ((i < 6) || (i > 11)) {
-          teaCardInfo.classList.toggle('hidden')
+    if ((i < 6) || (i > 11)) {
+      teaCardInfo.classList.toggle('hidden')
     }
-    })
-    filterCategoryCheckboxInput[2].addEventListener('click', () => {
-     
-      if ((i < 12) || (i > 17)) {
-        teaCardInfo.classList.toggle('hidden')
-      }
-      })
-      filterCategoryCheckboxInput[3].addEventListener('click', () => {
-        if (i < 18) {
-          teaCardInfo.classList.toggle('hidden')
-        }
-        })
+  })
+  filterCategoryCheckboxInput[2].addEventListener('click', () => {
+    if ((i < 12) || (i > 17)) {
+      teaCardInfo.classList.toggle('hidden')
+    }
+  })
+  filterCategoryCheckboxInput[3].addEventListener('click', () => {
+    if (i < 18) {
+      teaCardInfo.classList.toggle('hidden')
+    }
+  })
+
+
   const teaCard = teaCardInfo.appendChild(document.createElement(`div`)) as HTMLDivElement;
   teaCard.classList.add('tea-card');
   teaCard.style.backgroundImage =  `url(${listBuys[i].image1})`;
@@ -176,7 +297,6 @@ for (let i = 0; i < 24; i++) {
       priceGramm = amountNumber * listBuys[i].price;
       amount.textContent= `${amountNumber} gramm`;
       price.textContent = `${priceGramm} $`;
-      
     }
   })
   
