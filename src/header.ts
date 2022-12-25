3f26969e6edfb5189d7059b6104973e2cf875fc6
Mariya Petrovskaya <pma190183@yandex.ru>
2022-12-25 19:13:06 +0300

    feat: add blocks

---
+++ src/header.ts
@@ -0,0 +1,56 @@
+import "./header.css";
+import "./index.css";
+
+import "./footer.css";
+
+const body = document.querySelector('body') as HTMLBodyElement;
+
+const header = body.appendChild(document.createElement(`header`)) as HTMLElement;
+
+const nav = header.appendChild(document.createElement(`nav`)) as HTMLElement;
+for ( let i = 0; i<3; i++) {
+  const navLink = nav.appendChild(document. createElement(`li`)) as HTMLLIElement;
+  navLink.classList.add('nav-link')
+}
+const navLi = document.querySelectorAll('.nav-link') as NodeListOf<HTMLLIElement>
+const navLink1 = navLi[0].appendChild(document. createElement(`a`)) as HTMLAnchorElement;
+navLink1.classList.add('logo-link');
+navLink1.href="https://online-store-tea-time.netlify.app/"
+const teaTimeLogo = navLink1.appendChild(document. createElement(`div`)) as HTMLDivElement;
+teaTimeLogo.classList.add('tea-time-logo')
+const teaTimeLogoH1 = navLink1.appendChild(document. createElement(`div`)) as HTMLDivElement;
+teaTimeLogoH1.classList.add('tea-time-logo-h1');
+teaTimeLogoH1.textContent = 'Tea-time';
+
+
+const navLink2 = navLi[1].appendChild(document. createElement(`form`)) as HTMLFormElement;
+navLink2.method="get"
+const inputNavLink2 = navLink2.appendChild(document. createElement(`input`)) as HTMLInputElement;
+
+inputNavLink2.name = 's'
+inputNavLink2.placeholder = 'Search tea...'
+inputNavLink2.type = "search"
+
+
+const button = navLink2.appendChild(document. createElement(`button`)) as HTMLButtonElement;
+button.classList.add('button');
+button.type = 'submit';
+
+const buttonIcon = button.appendChild(document. createElement(`div`)) as HTMLDivElement;
+buttonIcon.classList.add('search-icon')
+
+const navLink3 = navLi[2].appendChild(document. createElement(`a`)) as HTMLAnchorElement;
+navLink3.href="item.html"
+navLink3.classList.add('logo-link');
+
+const totalPrice = navLink3.appendChild(document. createElement(`span`)) as HTMLSpanElement;
+totalPrice.textContent = 'Total-price: 00';
+
+const headerCart = navLink3.appendChild(document. createElement(`div`)) as HTMLDivElement;
+headerCart.classList.add('header-cart')
+
+const amountCart = navLink3.appendChild(document. createElement(`div`)) as HTMLDivElement;
+amountCart.classList.add('amount-buys');
+amountCart.textContent = '0';
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,14 @@
+import "./header.css";
+import "./header.ts";
+
 import "./index.css";
+
+import "./footer.css";
+import "./footer.ts";
+
 import listBuys from "./list-buys";
 
 const body = document.querySelector('body') as HTMLBodyElement;
-
-
-const header = body.appendChild(document.createElement(`header`)) as HTMLElement;
-const nav = header.appendChild(document.createElement(`nav`)) as HTMLElement;
-for ( let i = 0; i<3; i++) {
-  const navLink = nav.appendChild(document. createElement(`li`)) as HTMLLIElement;
-  navLink.classList.add('nav-link')
-}
-const navLi = document.querySelectorAll('.nav-link') as NodeListOf<HTMLLIElement>
-const navLink1 = navLi[0].appendChild(document. createElement(`a`)) as HTMLAnchorElement;
-navLink1.classList.add('logo-link');
-navLink1.href="https://online-store-tea-time.netlify.app/"
-const teaTimeLogo = navLink1.appendChild(document. createElement(`div`)) as HTMLDivElement;
-teaTimeLogo.classList.add('tea-time-logo')
-const teaTimeLogoH1 = navLink1.appendChild(document. createElement(`div`)) as HTMLDivElement;
-teaTimeLogoH1.classList.add('tea-time-logo-h1');
-teaTimeLogoH1.textContent = 'Tea-time';
-
-
-const navLink2 = navLi[1].appendChild(document. createElement(`form`)) as HTMLFormElement;
-navLink2.method="get"
-const inputNavLink2 = navLink2.appendChild(document. createElement(`input`)) as HTMLInputElement;
-
-inputNavLink2.name = 's'
-inputNavLink2.placeholder = 'Search tea...'
-inputNavLink2.type = "search"
-
-
-const button = navLink2.appendChild(document. createElement(`button`)) as HTMLButtonElement;
-button.classList.add('button');
-button.type = 'submit';
-
-const buttonIcon = button.appendChild(document. createElement(`div`)) as HTMLDivElement;
-buttonIcon.classList.add('search-icon')
-
-const navLink3 = navLi[2].appendChild(document. createElement(`a`)) as HTMLAnchorElement;
-navLink3.href="item.html"
-navLink3.classList.add('logo-link');
-
-const totalPrice = navLink3.appendChild(document. createElement(`span`)) as HTMLSpanElement;
-totalPrice.textContent = 'Total-price: 00';
-
-const headerCart = navLink3.appendChild(document. createElement(`div`)) as HTMLDivElement;
-headerCart.classList.add('header-cart')
-
-const amountCart = navLink3.appendChild(document. createElement(`div`)) as HTMLDivElement;
-amountCart.classList.add('amount-buys');
-amountCart.textContent = '0';
 
 const main = body.appendChild(document.createElement(`main`)) as HTMLElement;
 const containerFiltres = main.appendChild(document.createElement(`div`)) as HTMLDivElement;
@@ -92,9 +51,7 @@ const filterCategoryCheckboxInput = document.querySelectorAll('.filter-input') a
 const filterCategoryCheckboxLabel= document.querySelectorAll('.filter-label') as NodeListOf<HTMLLabelElement>;
 
 filterCategoryCheckboxInput[0].id = 'green-tea'
-// filterCategoryCheckboxInput[0].name = 'green-tea';
 filterCategoryCheckboxLabel[0].setAttribute('for', `green-tea`);
-// filterCategoryCheckboxLabel[0].setAttribute('name', `green-tea`);
 filterCategoryCheckboxLabel[0].textContent = 'Green tea';
 
 filterCategoryCheckboxInput[1].id = 'Oolong'
@@ -244,42 +201,4 @@ for (let i = 0; i < 24; i++) {
 
 
 
-//footer
-const footer = body.appendChild(document.createElement(`footer`)) as HTMLElement;
-footer.classList.add('footer')
-const footerContainer = footer.appendChild(document.createElement(`div`)) as HTMLDivElement;
-footerContainer.classList.add('footer-container');
-const footerList = footerContainer.appendChild(document.createElement(`ul`)) as HTMLUListElement;
-footerList.classList.add('footer-list');
- for (let i=0; i<3; i++) {
-  const footerListLi = footerList.appendChild(document.createElement(`li`)) as HTMLLIElement;
-  footerListLi.classList.add('footer-item');
-  const footerListLink = footerListLi.appendChild(document.createElement(`a`)) as HTMLAnchorElement;
-  footerListLink.classList.add('footer-link');
-  const footerListImage = footerListLink.appendChild(document.createElement(`img`)) as HTMLImageElement;
-  footerListImage.classList.add('footer-image');
-  const footerListP= footerListLink.appendChild(document.createElement(`p`)) as HTMLParagraphElement;
-  footerListP.classList.add('footer-p');
- 
- }
- const footerLi = document.querySelectorAll('.footer-item') as NodeListOf<HTMLLIElement>
- const footerLink = document.querySelectorAll('.footer-link') as NodeListOf<HTMLAnchorElement>
- const footerP = document.querySelectorAll('.footer-p') as NodeListOf<HTMLParagraphElement>
- const footerImage = document.querySelectorAll('.footer-image') as NodeListOf<HTMLImageElement>
 
-footerLink[0].href = 'https://github.com/Mary190183/'
-footerP[0].textContent = 'Mary190183'
-
-footerLink[1].href = 'https://github.com/balabanova-iu/'
-footerP[1].textContent = 'balabanova-iu'
-
-footerLink[2].href = 'https://rs.school/js/'
-footerP[2].textContent = '2022-2023'
-
-footerImage[0].src = './assets/icon/github.svg';
-footerImage[0].alt = 'github'
-footerImage[1].src = './assets/icon/github.svg';
-footerImage[1].alt = 'github'
-footerImage[2].src = './assets/icon/rsschool.svg'
-footerImage[2].classList.add('rsschool')
-footerImage[2].alt = 'rsschool'
