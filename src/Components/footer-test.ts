7a088e6d0e3f79cfeae0ba57dabead8229706078
balabanova-iu <balabanova.iu@gmail.com>
2022-12-26 13:23:28 +0300

    refactor: change class names

--- src/Components/footer-test.ts
+++ src/Components/footer-test.ts
@@ -3,19 +3,19 @@ import "./footer-test.css";
 function renderFooter() {
   return `
   <footer class="footer">
-      <div class="footer-container">
-        <ul class="footer-list">
-          <li class="footer-item">
-            <img src="assets/icon/github.svg" class="masha-github" alt="github" style="width: 30px" />
-            <a class="footer-link" href="https://github.com/Mary190183/">Mary190183</a>
+      <div class="footer__container">
+        <ul class="footer__list">
+          <li class="footer__item">
+            <img src="assets/icon/github.svg" class="footer__masha-github" alt="github" style="width: 30px" />
+            <a class="footer__link" href="https://github.com/Mary190183/">Mary190183</a>
           </li>
-          <li class="footer-item">
-            <img src="assets/icon/github.svg" class="masha-github" alt="github" style="width: 30px" />
-            <a class="footer-link" href="https://github.com/balabanova-iu/">balabanova-iu</a>
+          <li class="footer__item">
+            <img src="assets/icon/github.svg" class="footer__masha-github" alt="github" style="width: 30px" />
+            <a class="footer__link" href="https://github.com/balabanova-iu/">balabanova-iu</a>
           </li>
-          <li class="footer-item">
+          <li class="footer__item">
             <a href="https://rs.school/js/"
-              ><img src="assets/icon/rsschool.svg" class="rsschool" alt="rs_school" style="width: 70px"
+              ><img src="assets/icon/rsschool.svg" class="footer__rsschool" alt="rs_school" style="width: 70px"
             /></a>
           </li>
         </ul>
--- src/Components/header-test.ts
+++ src/Components/header-test.ts
@@ -2,27 +2,27 @@ import "./header-test.css";
 
 function renderHeader() {
   return `
-  <header>
-  <nav>
-    <li class="nav-link">
+  <header class="header">
+  <nav class="header__nav">
+    <li class="header__nav-link">
       <a href="https://online-store-tea-time.netlify.app/">
-        <div class="tea-time-logo"></div>
-        <h1 class="tea-time-logo-h1">Tea-time</h1>
+        <div class="header__tea-time-logo"></div>
+        <h1 class="header__tea-time-logo-h1">Tea-time</h1>
       </a>
     </li>
-    <li class="nav-link">
-      <form action="" method="get">
-        <input name="s" placeholder="Search tea..." type="search" class="search-input" />
-        <button type="submit" class="submit-button">
-          <div class="search-icon"></div>
+    <li class="header__nav-link">
+      <form action="" method="get" class="header__form">
+        <input name="s" placeholder="Search tea..." type="search" class="header__search-input" />
+        <button type="submit" class="header__submit-button">
+          <div class="header__search-icon"></div>
         </button>
       </form>
     </li>
-    <li class="nav-link">
+    <li class="header__nav-link">
       <a href="item.html">
         <span>total-price: 00</span>
-        <div class="header-cart"></div>
-        <div class="amount-buys">0</div>
+        <div class="header__cart"></div>
+        <div class="header__amount-buys">0</div>
       </a>
     </li>
   </nav>
