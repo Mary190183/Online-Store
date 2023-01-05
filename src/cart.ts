30319108f07f3f71d4e4c80f047d3d3387149919
balabanova-iu <balabanova.iu@gmail.com>
2023-01-06 00:21:13 +0300

    feat: add pagination

--- src/cart.ts
+++ src/cart.ts
@@ -18,7 +18,7 @@ window.onload = function () {
   window.cartController.updateView();
 };
 
-function render({ items, totalPrice, totalAmount }: CartModel) {
+function render({ items, totalPrice, totalAmount, page, limitItems }: CartModel) {
   return `
   ${renderHeader({ totalAmount, totalPrice })}
     <section class="section-content">
@@ -32,23 +32,27 @@ function render({ items, totalPrice, totalAmount }: CartModel) {
             <button class="checkout-button">Checkout</button>
           </div>
         </div>
-        ${items.map((item) => {
-          const product = listBuys.find((prod) => prod.id === item.productId);
+        ${
+          items.length > 0
+            ? items.map((item) => {
+                const product = listBuys.find((prod) => prod.id === item.productId);
 
-          if (product) {
-            return renderCartItem({
-              image: product.image1,
-              sort: product.sort,
-              province: product.province,
-              price: product.price * item.amount,
-              id: item.id,
-              amount: item.amount,
-              stock: product.stock,
-            });
-          } else {
-            return "";
-          }
-        })}
+                if (product) {
+                  return renderCartItem({
+                    image: product.image1,
+                    sort: product.sort,
+                    province: product.province,
+                    price: product.price * item.amount,
+                    id: item.id,
+                    amount: item.amount,
+                    stock: product.stock,
+                  });
+                } else {
+                  return "";
+                }
+              })
+            : `<h2 class="empty">Empty</h2>`
+        }
         <div class="section-end">
           <div class="promo">
             <div class="promo-text">Promo code</div>
@@ -56,12 +60,16 @@ function render({ items, totalPrice, totalAmount }: CartModel) {
             <button class="promo-activate">Activate</button>
           </div>
           <div class="pages">
+            <div class="limit">
+              <div>Limit</div>
+              <input type="number" min="1" max="10" step="1" value="${limitItems}" onchange="cartController.updateLimitItems(event)" class="limit-input"/>
+            </div>
             <div class="pages-text">Page</div>
-            <button class="button-left">
+            <button class="button-left" onclick="cartController.onclickLeft()">
               <img src="./assets/svg/arrow-right.svg" alt="arrow-left" class="pages-img-left" />
             </button>
-            <div class="cart-page">1</div>
-            <button class="button-right">
+            <div class="cart-page">${page}</div>
+            <button class="button-right" onclick="cartController.onclickRight()">
               <img src="./assets/svg/arrow-right.svg" alt="arrow-right" class="pages-img-right" />
             </button>
           </div>
--- src/models/cart-model.ts
+++ src/models/cart-model.ts
@@ -2,6 +2,8 @@ import listBuys from "../list-buys";
 
 class CartModel {
   private _items: CartItem[];
+  public page: number;
+  public limitItems: number;
 
   public constructor() {
     this._items = [
@@ -20,11 +22,18 @@ class CartModel {
         productId: 18,
         amount: 3,
       },
+      {
+        id: 4,
+        productId: 10,
+        amount: 3,
+      },
     ];
+    this.page = 1;
+    this.limitItems = 2;
   }
 
   public get items() {
-    return this._items;
+    return this._items.slice((this.page - 1) * this.limitItems, this.page * this.limitItems);
   }
 
   public get totalPrice() {
@@ -50,6 +59,10 @@ class CartModel {
 
   public deleteItemById(id: number) {
     this._items = this._items.filter((item) => item.id !== id);
+
+    if (this.items.length === 0 && this.page > 1) {
+      this.page -= 1;
+    }
   }
 
   public updateAmountById(id: number, amount: number) {
@@ -62,6 +75,27 @@ class CartModel {
         item.amount = amount;
       }
     });
+  }
+
+  public updateLimitItemsOnPage(limitItems: number) {
+    this.limitItems = limitItems;
+    this.page = 1;
+  }
+
+  public onclickRightPageNumber() {
+    if (this._items.length - 1 < this.page * this.limitItems) {
+      this.page;
+    } else {
+      this.page = this.page + 1;
+    }
+  }
+
+  public onclickLeftPageNumber() {
+    if (this.page - 1 === 0) {
+      this.page = 1;
+    } else {
+      this.page = this.page - 1;
+    }
   }
 }
 
